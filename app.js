3ba0a23735ade5e4d3fbda9248dc9c048a0e3132
mmq <527148183@qq.com>
2019-06-02 20:59:35 +0800

    vue练习

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+// 实例化vue对象
+new Vue({
+  el: '#vue-app',
+  data: {
+    name: 'Mad.Max'
+  }
+})
+
+
+// var app = new Vue({ 
+//     el: '#app',
+//     data: {
+//         message: 'Hello Vue!'
+//     }
+// });
