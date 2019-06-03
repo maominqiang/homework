076bfebee6c59b4105378421f536fb96d82a2dc4
mmq <527148183@qq.com>
2019-06-03 20:36:21 +0800

    lesson3:创建vue容器，属性绑定

--- app.js
+++ app.js
@@ -2,8 +2,24 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: 'Mad.Max'
+    name: 'Mad.Max',
+	website:"https://www.baidu.com",
+	websiteTag:"<a href='https://baidu.com'>百度</a>"
+  },
+  methods:{
+	hello:function(Str){
+		var h = new Date().getHours();
+		if(h >= 19){
+			return "Good evening ," + Str;
+		}else if(h >= 12){
+			return "Good Afternoon ," + Str;
+		}else{
+			return "Good Morning ," + Str;
+		} 
+	}
+	  
   }
+
 })
 
 
