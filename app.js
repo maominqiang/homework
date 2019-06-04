0727adfca65455bda2d5356e77dedf979c2b4904
mmq <527148183@qq.com>
2019-06-04 22:33:15 +0800

    数据双向绑定

--- app.js
+++ app.js
@@ -2,14 +2,17 @@
 new Vue({
   el: '#vue-app',
   data: {
-
+	name:"",
+	age:0
   },
   methods:{
 	  logname:function(){
-		  console.log("正在输入姓名");
+		  // console.log(this.$refs.name.value);
+		  // this.name = this.$refs.name.value;
 	  },
 	  logage:function(){
-		  console.log("正在输入年龄");
+		  // console.log("正在输入年龄");
+		  this.age= this.$refs.age.value;
 	  }
   }
 
